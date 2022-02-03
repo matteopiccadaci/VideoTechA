02d53eb66ee8655d1e5dc1eb9f44d3f896eafed6
mastronardo <mstnnn01r17f158x@studenti.unime.it>
2022-02-03 10:50:07 +0100

    modifiche mastronardo

--- README.md
+++ README.md
@@ -1,17 +1,2 @@
-# Progetto Basi di Dati
-## VideoTech-A
-
-_Studenti:_
-Matteo Piccadaci [514430] e Antonino Mastronardo [513936]
-
-
-Si vuole realizzare una base di dati per una videoteca, il cui locale fisico è accessibile dal lunedì al sabato, dalle ore 8:30 alle ore 19:30.
-I contenuti messi a disposizione degli utenti sono Film e Album Musicali, di vario genere.
-I contenuti vengono messi in vendita ad un prezzo di listino prestabilito, al quale possono essere applicati sconti se il cliente ha un card fedeltà o se la sua età è compresa tra i 16 e i 24 anni.
-È altresì possibile noleggiare per un tempo determinato (in particolare tre giorni, una settimana o un mese) un massimo di 5 contenuti contemporaneamente ad un prezzo ridotto.
-Il cliente può sottoscrivere la card fedeltà se ha compiuto almeno il 18° anno di età: nel caso si fosse a disposizione di tale card, si avrà diritto al 10% di sconto per spese superiori a €30.
-Se l’età del cliente rientra in quella vista sopra, avrà diritto al 5% di sconto su qualunque spesa.
-I due sconti, tuttavia, non sono cumulabili: il cliente dovrà in ogni caso pagare la cifra minore.
-I clienti, se hanno compiuto almeno il 16° di età, potranno autenticarsi tramite codice univoco e password al momento del log-in al sito tramite apposito pulsante. È invece previsto un altro pulsante d’accesso per gli amministratori dell’esercizio, i quali dovranno autenticarsi tramite credenziali, che potranno visualizzare gli articoli in noleggio (compresa la data di noleggio e la durata di quest’ultimo) e le scorte presenti in magazzino fra gli altri.
-È anche previsto un accesso in modalità guest, senza alcun tipo di autenticazione, nella quale sarà possibile accedere solamente alle informazioni riguardo l’esercizio e al catalogo degli articoli.
+# VideoTechA
 
--- registrazione.php
+++ registrazione.php
@@ -1,58 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Registrazione</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap">
-    <link rel="stylesheet" href="src/css/style.css">
-</head>
-<body>
-<form method="post" action="registrazione.php">
-    <h1>Registrazione</h1>
-    <input type="nome" id="nome" placeholder="Nome" name="nome" required>
-    <input type="cognome" id="cognome" placeholder="Cognome" name="cognome" required>
-    <div class="input-group mb-3">
-        <input type="text"  id="mail" class="form-control" placeholder="Inserisci la tua mail" aria-label="Username" name="mail" aria-describedby="inputGroup-sizing-sm" maxlength="50" required>
-    </div>
-    <input type="password" id="password" placeholder="Password" name="password" required>
-    <input type="date" id="data" placeholder="Data" name="data" required>
-    <button type="button" class="btn btn-outline-dark" name="register">Registrati</button>
-</form>
-<body class="text-center">
-
-<main class="form-signin">
-    <form>
-        <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email address</label>
-        </div>
-        <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-            <label for="floatingPassword">Password</label>
-        </div>
-
-        <div class="checkbox mb-3">
-            <label>
-                <input type="checkbox" value="remember-me"> Remember me
-            </label>
-        </div>
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
-    </form>
-</main>
-
-
-
-</body>
-</html>
-</body>
-</html>
 <?php
 require_once ("php/cofig.php");
 
-if (isset($_POST['register'])) {
+if (isset($_POST['registrati'])) {
     $mail = $_POST['mail'];
     $password = $_POST['password'];
     $nome = $_POST['nome'];
@@ -62,22 +11,120 @@ if (isset($_POST['register'])) {
     $data_t= explode("-", $data);
     $eta=(date("md", date("U", mktime(0,0,0, $data_t[2], $data_t[1], $data_t[0]))) > date("md") ? ((date("Y")-$data_t[0])-1) : (date("Y") - $data_t[0]));
     if (empty($username) || empty($password) || empty($nome) || empty($cognome) || empty($data)) {
-        $msg.= 'Compila tutti i campi %s';
+        $msg= 'Compila tutti i campi %s';
     } elseif ($pwdLenght < 4 || $pwdLenght > 20) {
-       echo "<script> alert ('Registrazione non avvenuta: La lunghezza della password deve essere compresa fra 4 e 20 caratteri')</script>";}
+        echo "<script> alert ('Registrazione non avvenuta: La lunghezza della password deve essere compresa fra 4 e 20 caratteri')</script>";}
 
     elseif ($eta<16){
         echo "<script> alert('Registrazione non avvenuta: L\'età per registrarsi è di almeno 16 anni') </script>";
-    }
+    } //errori
+
+
     else {
         $password_hash = password_hash($password, PASSWORD_BCRYPT);// hashing password
-    $query = "
+        $query = "
                 INSERT INTO Clienti  (nome, cognome,mail, pass, data_nascita)
                 VALUES ('$nome', '$cognome', '$mail', '$password_hash', '$data')
             ";
-    if ($result=$connex_db->query($query)){
-        echo "<script> alert('Registrazione avvenuta')</script>";
+        if ($result=$connex_db->query($query)){
+            header("location: /loginclienti.php");
+            echo "<script> alert('Registrazione avvenuta!')</script>";
 
-    }
-}}
+        }
+    } //registrazione avvenuta
+
+}
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Registrazione</title>
+
+
+    <!-- AdminLTE v3 core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+
+
+    <!-- JS ADMIN LTE V3-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
+
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#7952b3">
+
+    <!-- Custom styles for this template -->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <div class="login-logo">
+        <a href="../../index2.html"><b>VideoTech-A</b></a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">Crea il tuo nuovo account</p>
+            <form method="post" action="registrazione.php">
+            <div class="row g-3">
+                <div class="col-md-6">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" class="form-control" id="nome" placeholder="Nome" name="nome">
+                </div>
+                <div class="col-md-6">
+                    <label for="cognome" class="form-label">Cognome</label>
+                    <input type="text" class="form-control" id="cognome" placeholder="Cognome" name="cognome">
+                </div>
+                <br>
+                <div class="col-12">
+                    <label for="mail" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="mail" placeholder="Mail" name="mail" maxlength="50">
+                </div>
+                <div class="col-12">
+
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control"  id="password" placeholder="Password" name="password" maxlenght="20">
+                </div>
+                <div class="col-12">
+                     <label for="data" class="form-label">Data di nascita</label>
+                <input type="date" class="form-control" id="data" placeholder="Data" name="data">
+            </div>
+                <input type="text" id="registrati" name="registrati" hidden value="1">
+                <button  type="submit" name="registrati" class="btn btn-primary"> Registrati </button>
+            </div>
+            </form>
+                <div class="col-12">
+                    <br>
+                    <h6>Sei già registrato? <a href="loginclienti.php">Accedi qui</a></h6>
+                </div>
+
+                <div class="col-12">
+                    <h6>Altrimenti, accedi come <a href="guest.php">ospite</a></h6>
+                </div>
+
+
+    </div>
+    <!-- /.login-card-body -->
+</div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+
+</body>
+</html>
